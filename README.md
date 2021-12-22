978d68d7373e12987c4f433d588e6f291bca1ebe
GiancarloRizzo <rizzo.giancarlo@t-online.de>
2021-12-22 22:21:55 +0100

    Create inital version of multiple-imu-reader

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # multiple-mpu9250-reader
 Reading up to 4 mpu9250 with Raspberry 3B+ using i2c
+
+## Wiring 
+To use the Raspi3B+ and upt four MPU9250 you need to use two i2c-busses like described in this [HowTo](https://github.com/GiancarloRizzo/HowToSummeries/blob/master/HowTo-Raspberry/how-to-connect-multiple-MPU9250-with-raspi.md).
+
+## Pre: Environment and dependencies
+```bash
+virtualenv -p python3 
+```
+
+## Code
+In your code you need to reference the two busses and the two devices by their address for each bus:
+```
+#bus  address name
+bus_0 0x68    imu11
+bus_0 0x69    imu12 
+bus_1 0x68    imu21
+bus_1 0x69    imu22
+```
+
+## Start example
+```
+python3 ./src/imu_reader.py
+# calibrates imus and creates imu.log 
+```
---
+++ src/mpu_reader.py
@@ -0,0 +1,83 @@
+import os
+import sys
+import time
+import smbus
+
+from imusensor.MPU9250 import MPU9250
+
+filename = 'mpu.log'
+
+address_master = 0x68
+address_slave = 0x69
+
+bus_1 = smbus.SMBus(0)
+bus_2 = smbus.SMBus(1)
+
+imu_1_master = MPU9250.MPU9250(bus_1, address_master)
+imu_1_slave = MPU9250.MPU9250(bus_1, address_slave)
+imu_2_master = MPU9250.MPU9250(bus_2, address_master)
+imu_2_slave = MPU9250.MPU9250(bus_2, address_slave)
+
+# ranges: +-2G +-4G +-8G +-16G
+imu_1_master.setAccelRange("AccelRangeSelect2G")
+imu_1_slave.setAccelRange("AccelRangeSelect2G")
+imu_2_master.setAccelRange("AccelRangeSelect2G")
+imu_2_slave.setAccelRange("AccelRangeSelect2G")
+
+
+#DPS:dagrees per second : +-250 +-500 +-1000 +-2000
+imu_1_master.setGyroRange("GyroRangeSelect250DPS")
+imu_1_slave.setGyroRange("GyroRangeSelect250DPS")
+imu_2_master.setGyroRange("GyroRangeSelect250DPS")
+imu_2_slave.setGyroRange("GyroRangeSelect250DPS")
+
+# lowpassfreq: 5Hz, 10Hz, 20Hz, 41Hz, 92Hz, 184Hz
+imu_1_master.setLowPassFilterFrequency("AccelLowPassFilter184")
+imu_1_slave.setLowPassFilterFrequency("AccelLowPassFilter184")
+imu_2_master.setLowPassFilterFrequency("AccelLowPassFilter184")
+imu_2_slave.setLowPassFilterFrequency("AccelLowPassFilter184")
+
+imu_1_master.begin()
+imu_1_slave.begin()
+imu_2_master.begin()
+imu_2_slave.begin()
+
+imu_1_master.caliberateAccelerometer()
+imu_1_slave.caliberateAccelerometer()
+imu_2_master.caliberateAccelerometer()
+imu_2_slave.caliberateAccelerometer()
+
+imu_1_master.caliberateGyro()
+imu_1_slave.caliberateGyro()
+imu_2_master.caliberateGyro()
+imu_2_slave.caliberateGyro()
+
+
+logfile = open(filename, 'w')
+print ("""S1_master_accel_x,S1_master_accel_y,S1_master_accel_z,S1_slave_accel_x,S1_slave_accel_y,S1_slave_accel_z,S2_master_accel_x,S2_master_accel_y,S2_master_accel_z,S2_slave_accel_x,S2_slave_accel_y,S2_slave_accel_z,
+	S1_master_gyro_x,S1_master_gyro_y,S1_master_gyro_z,S1_slave_gyro_x,S1_slave_gyro_y,S1_slave_gyro_z,S2_master_gyro_x,S2_master_gyro_y,S2_master_gyro_z,S2_slave_gyro_x,S2_slave_gyro_y,S2_slave_gyro_z,
+	S1_master_mag_x,S1_master_mag_y,S1_master_mag_z,S1_slave_mag_x,S1_slave_mag_y,S1_slave_mag_z,S2_master_mag_x,S2_master_mag_y,S2_master_mag_z,S2_slave_mag_x,S2_slave_mag_y,S2_slave_mag_z,
+	""", file=logfile)
+logfile.close
+
+
+counter = 0;
+while counter < 100:
+	logfile = open(filename, 'a')
+
+	imu_1_master.readSensor()
+	imu_1_master.computeOrientation()
+	imu_1_slave.readSensor()
+	imu_1_slave.computeOrientation()
+	imu_2_master.readSensor()
+	imu_2_master.computeOrientation()
+	imu_2_slave.readSensor()
+	imu_2_slave.computeOrientation()
+
+	print (f"""{imu_1_master.AccelVals[0]},{imu_1_master.AccelVals[1]},{imu_1_master.AccelVals[2]},{imu_1_slave.AccelVals[0]},{imu_1_slave.AccelVals[1]},{imu_1_slave.AccelVals[2]},{imu_2_master.AccelVals[0]},{imu_2_master.AccelVals[1]},{imu_2_master.AccelVals[2]},{imu_2_slave.AccelVals[0]},{imu_2_slave.AccelVals[1]},{imu_2_slave.AccelVals[2]}
+	{imu_1_master.GyroVals[0]},{imu_1_master.GyroVals[1]},{imu_1_master.GyroVals[2]},{imu_1_slave.GyroVals[0]},{imu_1_slave.GyroVals[1]},{imu_1_slave.GyroVals[2]},{imu_2_master.GyroVals[0]},{imu_2_master.GyroVals[1]},{imu_2_master.GyroVals[2]},{imu_2_slave.GyroVals[0]},{imu_2_slave.GyroVals[1]},{imu_2_slave.GyroVals[2]}
+	{imu_1_master.MagVals[0]},{imu_1_master.MagVals[1]},{imu_1_master.MagVals[2]},{imu_1_slave.MagVals[0]},{imu_1_slave.MagVals[1]},{imu_1_slave.MagVals[2]},{imu_2_master.MagVals[0]},{imu_2_master.MagVals[1]},{imu_2_master.MagVals[2]},{imu_2_slave.MagVals[0]},{imu_2_slave.MagVals[1]},{imu_2_slave.MagVals[2]}""", file=logfile)
+
+	# print ("roll: {0} ; pitch : {1} ; yaw : {2}".format(imu_1_master.roll, imu_1_master.pitch, imu_1_master.yaw))
+	time.sleep(0.01)
+	counter = counter+1
